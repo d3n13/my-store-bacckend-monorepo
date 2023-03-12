55542680fa33167f166c9e86f9c565989caa9c6b
Denis Bilenko <goldenrodcap@gmail.com>
2023-03-13 03:45:12 +0400

    test importProductsFile

--- import-service/src/functions/importProductsFile/handler.ts
+++ import-service/src/functions/importProductsFile/handler.ts
@@ -1,27 +1,15 @@
-import { formatTextResponse } from "@libs/api-gateway";
 import { middyfy } from "@libs/lambda";
-import { s3Client } from "@libs/s3-client";
+import { importProductsFile } from "./importProductsFile";
+import {
+  formatTextResponse,
+  ValidatedEventAPIGatewayProxyEvent,
+} from "@libs/api-gateway";
 
-import type { ValidatedEventAPIGatewayProxyEvent } from "@libs/api-gateway";
-
-const AWS_S3_BUCKET_NAME = "import-service-csv-starage50830459";
-const AWS_SIGNED_KEY_EXPIRES_IN_SEC = 3600;
-
-const importProductsFile: ValidatedEventAPIGatewayProxyEvent<void> = async (
-  event
-) => {
+const handler: ValidatedEventAPIGatewayProxyEvent<void> = async (event) => {
   const fileName = event.queryStringParameters.name;
-  const key = `uploaded/${fileName}`;
-  const signedUrl = createPresignedUrlWithClient(key);
-  return formatTextResponse(signedUrl, event.headers.origin);
+  const origin = event.headers.origin;
+  const signedUrl = importProductsFile(fileName);
+  return formatTextResponse(signedUrl, origin);
 };
 
-function createPresignedUrlWithClient(key: string) {
-  return s3Client.getSignedUrl("putObject", {
-    Bucket: AWS_S3_BUCKET_NAME,
-    Key: key,
-    Expires: AWS_SIGNED_KEY_EXPIRES_IN_SEC,
-  });
-}
-
-export const main = middyfy(importProductsFile);
+export const main = middyfy(handler);
